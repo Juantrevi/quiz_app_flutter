5050c88dfdd5c1cf26d80931c8821abaa0ee039b
Juantrevi <juantrevi70@gmail.com>
2024-01-21 19:20:38 +0000

    -- Adding questions and buttons styling --

---
+++ lib/models/quiz_question.dart
@@ -0,0 +1,10 @@
+
+class QuizQuestion {
+
+  final String text;
+  final List<String> answers;
+
+  const QuizQuestion(this.text, this.answers);
+
+
+}
--- lib/question_screen.dart
+++ lib/question_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:quiz_app/answer_button.dart';
 
 class QuestionsScreen extends StatefulWidget {
   const QuestionsScreen({super.key});
@@ -10,6 +11,26 @@ class QuestionsScreen extends StatefulWidget {
 class _QuestionsScreenState extends State<QuestionsScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Text('Question string');
+    return SizedBox(
+      width: double.infinity,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text(
+            'Question 1',
+            style: TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+              color: Colors.white,
+            ),
+          ),
+          SizedBox(height: 20),
+          AnswerButton(answerText: 'answer 1', onTap: () {}),
+          AnswerButton(answerText: 'answer 2', onTap: () {}),
+          AnswerButton(answerText: 'answer 3', onTap: () {}),
+          AnswerButton(answerText: 'answer 4', onTap: () {}),
+        ],
+      ),
+    );
   }
 }
